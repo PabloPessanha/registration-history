585ee75e3f0ca97a16eb827ee005688ae15d5cab
Pablo <pablopessanh@gmail.com>
2021-06-20 19:49:51 -0300

    registration page desktop done

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { RegisterInputs } from '../../components';
+// import bgImage from '../../assets/bg-min.jpg';
+import styles from './styles.module.scss';
 
 export default function Register() {
   return (
-    <>
+    <div className={styles.homepage}>
       <RegisterInputs />
-    </>
+      <div className={styles.mainImage} />
+    </div>
   );
 }
