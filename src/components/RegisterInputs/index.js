a3b053278b585f76429534d077161dd05878707a
Pablo <pablopessanh@gmail.com>
2021-06-20 18:58:01 -0300

    inputs to register page done

--- src/components/RegisterInputs/index.js
+++ src/components/RegisterInputs/index.js
@@ -1,32 +1,54 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useCallback, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import styles from './styles.module.scss';
 import { cpfFormatter, phoneFormatter } from '../../utils';
 
 export default function RegisterInputs() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [cpf, setCpf] = useState('');
-  const [phone, setPhone] = useState('');
+  const [tel, setTel] = useState('');
+  const [verifyInfos, setVerifyInfos] = useState(false);
+  const history = useHistory();
 
-  function handleChange({ target }) {
+  const handleChange = useCallback(({ target }) => {
     switch (target.id) {
+      case 'name-input':
+        return setName(target.value);
+      case 'email-input':
+        return setEmail(target.value);
       case 'cpf-input':
         return setCpf(cpfFormatter(target.value));
       case 'tel-input':
-        return setPhone(phoneFormatter(target.value));
+        return setTel(phoneFormatter(target.value));
       default:
         return null;
     }
-  }
+  }, [name, email, cpf, tel]);
+
+  const handleSignup = useCallback((e) => {
+    e.preventDefault();
+    localStorage.setItem('logged', JSON.stringify({ name, email, cpf, tel }));
+    history.push('/users');
+  }, [name, email, cpf, tel]);
+
+  useEffect(() => {
+    const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+    if (name.length > 8 && regexEmail.test(email) && cpf.length >= 14 && tel.length >= 14) {
+      return setVerifyInfos(true);
+    }
+    return setVerifyInfos(false);
+  }, [name, email, cpf, tel]);
 
   return (
     <form className={styles}>
       <label htmlFor="name-input">
         Nome Completo
-        <input type="text" id="name-input" />
+        <input type="text" id="name-input" value={name} onChange={handleChange} />
       </label>
       <label htmlFor="email-input">
         E-mail
-        <input type="text" id="email-input" />
+        <input type="text" id="email-input" value={email} onChange={handleChange} />
       </label>
       <label htmlFor="cpf-input">
         CPF
@@ -34,10 +56,10 @@ export default function RegisterInputs() {
       </label>
       <label htmlFor="tel-input">
         Telefone
-        <input type="text" id="tel-input" value={phone} onChange={handleChange} maxLength="15" />
+        <input type="text" id="tel-input" value={tel} onChange={handleChange} maxLength="15" />
       </label>
       <div>
-        <button type="submit">Cadastrar</button>
+        <button type="submit" onClick={handleSignup} disabled={!verifyInfos}>Cadastrar</button>
         <Link to="/login">Login 🠖</Link>
       </div>
     </form>
