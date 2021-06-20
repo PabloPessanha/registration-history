137a8669600dcd080dc8975ff287154abe4a0b68
Pablo <pablopessanh@gmail.com>
2021-06-20 13:08:05 -0300

    fix routes; import global styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/globalStyles.scss';
 import Register from './pages/Register';
 
 function App() {
--- src/routes.js
+++ src/routes.js
@@ -8,8 +8,8 @@ export default function Routes() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={AllUsers} />
-        <Route path="/users" component={Register} />
+        <Route exact path="/" component={Register} />
+        <Route path="/users" component={AllUsers} />
         <Route path="/not-found" component={NotFound} />
         <Route path="*" component={() => <Redirect to="/not-found" />} />
       </Switch>
