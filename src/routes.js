ee1776501ae6dcef7f267e71f38049512cbc7e40
Pablo <pablopessanh@gmail.com>
2021-06-20 22:55:43 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AllUsers from './pages/AllUsers';
 import Register from './pages/Register';
 import NotFound from './pages/NotFound';
+import Login from './pages/Login';
 
 export default function Routes() {
   return (
@@ -10,8 +11,8 @@ export default function Routes() {
       <Switch>
         <Route exact path="/" component={Register} />
         <Route path="/users" component={AllUsers} />
-        <Route path="/not-found" component={NotFound} />
-        <Route path="*" component={() => <Redirect to="/not-found" />} />
+        <Route path="/login" component={Login} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </Router>
   );
